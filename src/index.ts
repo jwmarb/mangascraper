69113c4e5d1b4ac43cbfd3d5a11eec7181677de5
specify_ <dieptuber@gmail.com>
2021-06-26 02:57:57 -0700

    test

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-export function add(a: number, b: number) {
-  return a + b;
+export interface Manga {
+  id: string;
+  url: string;
+  title: string;
+  synopsis: string;
+  chapters: number;
+}
+
+export class Mangakakalot {
+  /**
+   * Get a list of manga that match the title
+   *
+   * @param title - Title of Manga (e.g Black Clover, One Piece, Naruto)
+   *
+   * @returns List of Manga that match `title`
+   */
+  public getMangaByTitle(title: string): Manga {
+    return { id: '123', url: 'youtube.com', title, synopsis: '', chapters: 10 };
+  }
 }
