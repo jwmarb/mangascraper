6b25b7076e0ddfe0c74c9075dc62bd55bc263d17
specify_ <dieptuber@gmail.com>
2021-07-06 14:06:41 -0700

    make headless chrome impossible to detect

--- src/functions/automateBrowser.ts
+++ src/functions/automateBrowser.ts
@@ -1,5 +1,6 @@
 import { Page } from 'puppeteer';
 import randomUserAgent from 'random-useragent';
+import preload from './preload';
 
 import puppeteer from 'puppeteer';
 import { AutomatedCallback, ScrapingOptions } from '..';
@@ -13,16 +14,26 @@ export default async function automateBrowser<T>(options: ScrapingOptions, callb
     args_proxy_server,
     '--no-sandbox',
     '--disable-setuid-sandbox',
+    '--disable-accelerated-2d-canvas',
+    '--no-zygote',
+    '--renderer-process-limit=1',
+    '--no-first-run',
+    '--ignore-certificate-errors',
+    '--ignore-certificate-errors-spki-list',
+    '--disable-dev-shm-usage',
     '--disable-infobars',
-    '--window-position=0,0',
-    '--ignore-certifcate-errors',
-    '--ignore-certifcate-errors-spki-list',
-    `--user-agent="${randomUserAgent.getRandom()}"`,
+    '--lang=en-US,en',
+    '--window-size=1920x1080',
+    '--disable-extensions',
+    `--user-agent=${randomUserAgent.getRandom((ua) => ua.osName === 'Windows' && ua.browserName === 'Chrome')}`,
   ].filter((item) => Boolean(item)) as string[];
 
   try {
-    const browser = await puppeteer.launch({ headless: !debug, args: puppeteer_args });
-    return await callback(browser).finally(async () => await browser.close());
+    const browser = await puppeteer.launch({ headless: !debug, args: puppeteer_args, ignoreHTTPSErrors: true });
+    const page = (await browser.pages())[0];
+    await page.setViewport({ width: 1920, height: 1080 });
+    await page.evaluateOnNewDocument(preload);
+    return await callback(page).finally(async () => await browser.close());
   } catch (e) {
     throw Error(e);
   }
