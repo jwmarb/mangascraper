bd2e0d894247580170537bc3d475324bfcf07dd6
specify_ <dieptuber@gmail.com>
2021-07-08 02:00:44 -0700

    added automation fallback

--- src/functions/readHtml.ts
+++ src/functions/readHtml.ts
@@ -4,8 +4,9 @@ import { ScrapingOptions } from '..';
 import puppeteer, { ElementHandle, Page } from 'puppeteer';
 import jqueryImport from 'jquery';
 import randomUserAgent from 'random-useragent';
+import automateBrowser from './automateBrowser';
 
-export default async function readHtml(url: string, options: ScrapingOptions = {}): Promise<CheerioAPI> {
+export default async function readHtml(url: string, options: ScrapingOptions): Promise<CheerioAPI> {
   const { proxy, debug = false } = options;
 
   if (debug) console.log(`Fetching HTML from ${url}`);
@@ -19,7 +20,16 @@ export default async function readHtml(url: string, options: ScrapingOptions = {
     if (debug) console.log(`Successfully retrieved html without any errors`);
     return cheerio.load(data);
   } catch (e) {
-    if (debug) console.log(`Failed to fetch HTML from ${url}. Maybe the request is being blocked by cloudflare?`);
-    throw Error(e);
+    if (debug) console.log(`Failed to fetch HTML from ${url}. Using puppeteer...`);
+    try {
+      const html = await automateBrowser(options, async (page) => {
+        await page.goto(url);
+        return await page.evaluate(() => document.querySelector('*')?.outerHTML || '');
+      });
+      if (debug) console.log('Successfully retrieved html after failed GET request');
+      return cheerio.load(html);
+    } catch (e) {
+      throw Error(e);
+    }
   }
 }
