d7c94e15de177a98710825a065e46321f6361edb
specify_ <dieptuber@gmail.com>
2021-07-08 00:35:42 -0700

    added user agent

--- src/functions/readHtml.ts
+++ src/functions/readHtml.ts
@@ -3,13 +3,19 @@ import axios from 'axios';
 import { ScrapingOptions } from '..';
 import puppeteer, { ElementHandle, Page } from 'puppeteer';
 import jqueryImport from 'jquery';
+import randomUserAgent from 'random-useragent';
 
 export default async function readHtml(url: string, options: ScrapingOptions = {}): Promise<CheerioAPI> {
   const { proxy, debug = false } = options;
 
   if (debug) console.log(`Fetching HTML from ${url}`);
   try {
-    const { data } = await axios.get(url, { proxy });
+    const { data } = await axios.get(url, {
+      proxy,
+      headers: {
+        'User-Agent': randomUserAgent.getRandom((ua) => ua.osName === 'Windows' && ua.browserName === 'Chrome'),
+      },
+    });
     if (debug) console.log(`Successfully retrieved html without any errors`);
     return cheerio.load(data);
   } catch (e) {
