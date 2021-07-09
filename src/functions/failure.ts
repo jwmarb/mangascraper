a26cd24c7bda84f975d85cf46b0710ee31dd0bc3
specify_ <dieptuber@gmail.com>
2021-07-09 00:51:32 -0700

    added console logging

--- src/functions/failure.ts
+++ src/functions/failure.ts
@@ -2,5 +2,6 @@ import { MangaCallback } from '../';
 
 export default function failure<T>(err: Error, callback?: MangaCallback<T>) {
   if (callback) return callback(err);
+  console.error(err);
   throw err;
 }
