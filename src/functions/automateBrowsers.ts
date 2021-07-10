74fec23786efa43a2ad72550e30fa1447bf23ab6
specify_ <dieptuber@gmail.com>
2021-07-10 11:50:59 -0700

    Added more functionality to `automatedBrowsers()`

--- src/functions/automateBrowsers.ts
+++ src/functions/automateBrowsers.ts
@@ -13,28 +13,51 @@ type Instances<T> = {
 export default async function automateBrowsers(options: ScrapingOptions, instances: Instances<any>[]): Promise<any> {
   const { proxy, debug = false, puppeteerInstance = { instance: 'default' } } = options;
 
-  const args_proxy_server = typeof proxy !== 'undefined' && `--proxy-server=${proxy.host}:${proxy.port}`;
+  const args_proxy_server = proxy != null && `--proxy-server=${proxy.host}:${proxy.port}`;
 
   const puppeteer_args = [...initPuppeteer.args, args_proxy_server].filter((item) => Boolean(item)) as string[];
 
   try {
-    const browser =
-      puppeteerInstance.instance === 'default'
-        ? await puppeteer.launch(
+    const browser = await (async () => {
+      switch (puppeteerInstance.instance) {
+        case 'endpoint':
+          return await puppeteer.connect({ browserWSEndpoint: puppeteerInstance.wsEndpoint });
+        case 'default':
+        default:
+          return await puppeteer.launch(
             puppeteerInstance.launch
-              ? puppeteerInstance.launch
+              ? {
+                  ...puppeteerInstance.launch,
+                  headless: !debug,
+                  args: puppeteerInstance.launch.args
+                    ? ([...puppeteerInstance.launch.args, args_proxy_server].filter((item) =>
+                        Boolean(item),
+                      ) as string[])
+                    : ([args_proxy_server].filter((item) => Boolean(item)) as string[]),
+                }
               : { ...initPuppeteer, headless: !debug, args: puppeteer_args },
-          )
-        : await puppeteer.connect({ browserWSEndpoint: puppeteerInstance.wsEndpoint });
-    const pages = await browser.pages();
+          );
+        case 'custom':
+          return puppeteerInstance.browser;
+      }
+    })();
+
     return Promise.all(
       instances.map(async ({ network, callback }, index) => {
-        const page = pages[index] ? pages[index] : await browser.newPage();
+        const page = await (async () => {
+          switch (puppeteerInstance.instance) {
+            case 'default':
+              return (await browser.pages())[0];
+            case 'custom':
+            default:
+              return await browser.newPage();
+          }
+        })();
         await page.setViewport({ width: 1920, height: 1080 });
         await page.evaluateOnNewDocument(preload);
 
         await page.setRequestInterception(true);
-        await page.on('request', (request) => {
+        page.on('request', (request) => {
           if (network != null) {
             if (network.resource) {
               switch (network.resource.method) {
@@ -59,13 +82,35 @@ export default async function automateBrowsers(options: ScrapingOptions, instanc
         });
 
         return callback(page).finally(async () => {
-          if (puppeteerInstance.instance === 'server' && index !== 0) await page.close();
-          else await page.close();
+          switch (puppeteerInstance.instance) {
+            case 'default':
+              await page.close();
+              break;
+            case 'endpoint':
+              await page.close();
+              break;
+            case 'custom':
+              if (puppeteerInstance.options?.closeAfterOperation) await page.close();
+              else if ((await browser.pages()).length > 1) await page.close();
+          }
         });
       }),
     ).finally(async () => {
-      if (puppeteerInstance.instance === 'default') await browser.close();
-      else browser.disconnect();
+      switch (puppeteerInstance.instance) {
+        case 'default':
+          await Promise.all((await browser.pages()).map((page) => page.close()));
+          await browser.close();
+          break;
+        case 'endpoint':
+          browser.disconnect();
+          break;
+        case 'custom':
+          if ((await browser.pages()).length <= 1 && puppeteerInstance.options?.closeAfterOperation) {
+            await Promise.all((await browser.pages()).map((page) => page.close()));
+            await browser.close();
+          }
+          break;
+      }
     });
   } catch (e) {
     throw Error(e);
