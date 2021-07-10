81110f7cbd331315ce1976f00b253c69cdb94837
specify_ <dieptuber@gmail.com>
2021-07-10 11:50:50 -0700

    Added more functionality to `automatedBrowser()`

--- src/functions/automateBrowser.ts
+++ src/functions/automateBrowser.ts
@@ -35,6 +35,8 @@ export interface BrowserNetworkOptions {
   };
 }
 
+let queue = [];
+
 export default async function automateBrowser<T>(
   options: ScrapingOptions,
   callback: AutomatedCallback<T>,
@@ -42,24 +44,48 @@ export default async function automateBrowser<T>(
 ) {
   const { proxy, debug = false, puppeteerInstance = { instance: 'default' } } = options;
 
-  const args_proxy_server = typeof proxy !== 'undefined' && `--proxy-server=${proxy.host}:${proxy.port}`;
+  const args_proxy_server = proxy != null && `--proxy-server=${proxy.host}:${proxy.port}`;
 
   const puppeteer_args = [...initPuppeteer.args, args_proxy_server].filter((item) => Boolean(item)) as string[];
 
   try {
-    const browser =
-      puppeteerInstance.instance === 'default'
-        ? await puppeteer.launch(
+    const browser = await (async () => {
+      switch (puppeteerInstance.instance) {
+        case 'endpoint':
+          return await puppeteer.connect({ browserWSEndpoint: puppeteerInstance.wsEndpoint });
+        case 'default':
+        default:
+          return await puppeteer.launch(
             puppeteerInstance.launch
-              ? puppeteerInstance.launch
+              ? {
+                  ...puppeteerInstance.launch,
+                  headless: !debug,
+                  args: puppeteerInstance.launch.args
+                    ? ([...puppeteerInstance.launch.args, args_proxy_server].filter((item) =>
+                        Boolean(item),
+                      ) as string[])
+                    : ([args_proxy_server].filter((item) => Boolean(item)) as string[]),
+                }
               : { ...initPuppeteer, headless: !debug, args: puppeteer_args },
-          )
-        : await puppeteer.connect({ browserWSEndpoint: puppeteerInstance.wsEndpoint });
-    const page = (await browser.pages())[0];
+          );
+        case 'custom':
+          return puppeteerInstance.browser;
+      }
+    })();
+
+    const page = await (async () => {
+      switch (puppeteerInstance.instance) {
+        case 'default':
+          return (await browser.pages())[0];
+        case 'custom':
+        default:
+          return await browser.newPage();
+      }
+    })();
     await page.setViewport({ width: 1920, height: 1080 });
     await page.evaluateOnNewDocument(preload);
     await page.setRequestInterception(true);
-    await page.on('request', (request) => {
+    page.on('request', (request) => {
       if (network != null) {
         if (network.resource) {
           switch (network.resource.method) {
@@ -83,8 +109,24 @@ export default async function automateBrowser<T>(
       request.continue();
     });
     return await callback(page).finally(async () => {
-      if (puppeteerInstance.instance === 'default') await browser.close();
-      else browser.disconnect();
+      switch (puppeteerInstance.instance) {
+        case 'default':
+          await Promise.all((await browser.pages()).map((page) => page.close()));
+          await browser.close();
+          break;
+        case 'endpoint':
+          await page.close();
+          browser.disconnect();
+          break;
+        case 'custom':
+          try {
+            await page.close();
+          } finally {
+            if ((await browser.pages()).length <= 1 && puppeteerInstance.options?.closeAfterOperation)
+              await browser.close();
+          }
+          break;
+      }
     });
   } catch (e) {
     throw Error(e);
